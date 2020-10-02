f9cd1f370d384d25600c9cdce377656b5bed64b7
Prusakov Maksim <prusakov.ms@phystech.edu>
2020-10-02 11:56:34 +0300

    1

--- 5работа/hare.py
+++ 5работа/hare.py
@@ -7,14 +7,24 @@ screen = pygame.display.set_mode((500, 600))
 
 white = (255, 255, 255)
 black = (0, 0, 0)
-gray = (42, 42, 42)
+gray = (180, 180, 180)
 marroon = (43, 17, 0)
 blue = (255, 0, 0)
+pink = ((255,100,180))
 
 screen.fill(white)
 
 
 def draw_body(x, y, width, height, colour):
+    '''
+    Функция рисует тело зайца
+    x, y - координаты центра изображения
+    width, height - ширина и высота изобажения
+    colour - цвет, заданный в формате, подходящем для pygame.Color
+    '''
+    pygame.draw.ellipse(screen, colour, (x - width // 2, y - height // 2, width, height))
+
+def draw_belly(x, y, width, height, colour):
     '''
     Функция рисует тело зайца
     x, y - координаты центра изображения
@@ -54,6 +64,16 @@ def draw_mouth(x, y, width, height, colour):
     pi = 3.14
     pygame.draw.arc(screen, colour, (x, y, width, height), pi, 2*pi)
 
+
+def draw_nose(x, y, size):
+    '''
+    Функция рисует нос зайца.
+    x, y - координаты носа
+    size - диаметр носа
+    colour - цвет, заданный в формате, подходящем для pygame.Color
+    '''
+    pygame.draw.circle(screen, pink, (x, y), size)
+
 def draw_leg(x, y, width, height, colour):
     '''
     Функция рисует ногу зайца.
@@ -85,6 +105,8 @@ def draw_hare(x, y, width, height, colour):
     body_y = y + body_height // 2
     draw_body(x, body_y, body_width, body_height, colour)
 
+    draw_belly(x, body_y, body_width // 1.5, body_height // 1.5, (150, 150, 150))
+
     head_size = height // 4
     draw_head(x, y - head_size // 2, head_size, colour)
 
@@ -96,6 +118,8 @@ def draw_hare(x, y, width, height, colour):
     draw_eye(x - head_size // 4, y - 3 * head_size // 4, head_size // 4, (0, 0, 255))
     draw_eye(x + head_size // 4, y - 3 * head_size // 4, head_size // 4, (0, 0, 255))
 
+    draw_nose(x + head_size // 30, y - head_size // 2, head_size // 30)
+
     draw_mouth(x-head_size // 3.5, y - head_size // 1.9, head_size // 3, head_size // 3, (255, 255, 255))
     draw_mouth(x+head_size // 25, y - head_size // 1.9, head_size // 3, head_size // 3, (255, 255, 255))
 
@@ -105,7 +129,7 @@ def draw_hare(x, y, width, height, colour):
         draw_leg(leg_x, leg_y, width // 4, leg_height, colour)
 
 
-draw_hare(250, 250, 250, 400, (42, 42, 42))
+draw_hare(250, 250, 250, 400, (100, 100, 100))
 
 
 pygame.display.update()
